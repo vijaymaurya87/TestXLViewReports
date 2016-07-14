69f4b1dedb0604d128a05062cd991cdb81d51f16
Vijay <vijay.akgec@gmail.com>
2016-07-14 12:47:06 +0530

    First commit

---
+++ src/test/java/com/test/TestReport.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.junit.Test;
+
+public class TestReport 
+{
+	@Test
+	public void testXLViewReport()
+	{
+		System.out.println("TEsting XLView Report");
+		System.out.println("It Got Passed");
+	}
+
+}
